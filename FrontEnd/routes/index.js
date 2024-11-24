77520bf33369fe59f94369fd1ebe3cb9faab624c
chris-codes-programs <epicnintendogamer500@gmail.com>
2024-11-24 18:02:12 -0500

    Better HUD (Somewhat implemented)
    Better HUD

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -3,8 +3,24 @@ const { check } = require('express-validator');
 const usersControllers = require('../controllers/users-controllers');
 const router = express.Router();
 
-router.get('/home', function(req,res,next)
+router.get('/', function(req,res,next)
 {
-    res.render("index");
+    res.render('./views/index');
 })
 
+router.get('/projects', function(req,res,next)
+{
+    res.render('./views/projects');
+})
+
+router.get('/signin', function(req,res,next)
+{
+    res.render('./views/signin');
+})
+
+router.get('/register', function(req,res,next)
+{
+    res.render('./views/register');
+})
+
+module.exports = router;
